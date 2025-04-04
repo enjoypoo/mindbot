659f7b61baae6103cd411c73a31cdb9d1a587a21
enjoypoo <enjoypoo@naver.com>
2025-04-04 12:10:18 +0900

    Update register-user.ts

--- api/register-user.ts
+++ api/register-user.ts
@@ -1,7 +1,7 @@
 // /api/register-user.ts
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../lib/prisma/client";
+import { prisma } from "@/lib/prisma/client";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
